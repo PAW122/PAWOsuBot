51409af360101bb202eecdd5b0124f6efb108cb1
PAWIU <70094237+PAW122@users.noreply.github.com>
2022-11-02 00:52:49 +0100

    add detaile

--- handlers/msg_handler.js
+++ handlers/msg_handler.js
@@ -9,15 +9,16 @@ const prefix = config.prefix
 
 module.exports = (client,message,user) => {
 
-    if(message.startsWith(prefix) && user.ircUsername != Username){//sprawdza prefix, && znaczy and
+    if(message.startsWith(prefix) && user.ircUsername != Username){//check is message start wits prefix
 
         const args = message.slice(prefix.length).trim().split(/ +/);
-        const commandName = args.shift().toLowerCase();//zwraca tylko 1 argument i zmienia na same małe liter
-        if(!client.command.has(commandName)) return;//sprawdza czy taka komenda istnieje
+        const commandName = args.shift().toLowerCase();//return first word and change to small leathers
+        if(!client.command.has(commandName)) return;//check is command with this name existing
         const time = new Date().toLocaleTimeString().slice(0,5)    
-        const command = client.command.get(commandName);//pobieramy komende
+        const command = client.command.get(commandName);//get command
     
         try {
+            //execute command
             command.execute(message,args,user,client);
             if(commands_logs == true){
                 const data = `${time} ${user.ircUsername} use ${commandName}, execute: "✅"`
